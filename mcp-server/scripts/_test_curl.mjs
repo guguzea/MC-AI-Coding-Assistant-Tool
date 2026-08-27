5e77d7bf7260a331cf349fc4a2ffdd488e02c318
guguzea <278391858@qq.com>
2026-08-27 21:20:05 +0800

    sweep43

--- mcp-server/scripts/_test_curl.mjs
+++
@@ -1,9 +0,0 @@
-const { execSync } = require("child_process");
-const url = process.argv[2] || "https://docs.neoforged.net/docs/concepts/registries/";
-const cmd = `curl -s -L -A "Mozilla/5.0" "${url}"`;
-const raw = execSync(cmd, { timeout: 30000, maxBuffer: 10 * 1024 * 1024 });
-const html = raw.toString("utf8");
-console.log("Length:", html.length);
-console.log("Has pre:", html.includes("<pre"));
-console.log("Has </pre>:", html.includes("</pre>"));
-if (html.length < 10000) console.log("TRUNCATED!");
